74d26b323081dd0767ba8d98dfd7d4037e92c8c3
Marcin <kielkowskimarcin@prokonto.pl>
2023-01-17 10:16:37 +0100

    update

--- list/examples/filter2.py
+++ list/examples/filter2.py
@@ -1,5 +1,10 @@
-list1 = [1,2,3,4]
-list2 = [4,5,6,7,8]
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-merged_list = list1 + list2
-print(merged_list)
+# returns True if number is even
+def check_even(number):
+    if number % 2 == 0:
+          return True  
+    return False
+        
+filtered_list = list(filter(check_even, list1))
+print(filtered_list)
